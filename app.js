fd4ff71a4146db558497ff68f9966f5f0d2a2bcd
Francisco Cesare <franciscocesare@MacBook-Pro-de-Francisco.local>
2022-05-29 01:46:23 -0300

    hice instalacion de nodemon, un html del index muy basico, algunas rutas de express, pase imagenes de brand a img para poder consumirlas, agregue normalize, y algunos estilos de css

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html')
+})
+app.get('/register', (req, res) => {  
+  res.sendFile(__dirname + '/views/register.html')
+})
+app.get('/product', (req, res) => {
+  res.sendFile(__dirname + '/views/product.html')
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
