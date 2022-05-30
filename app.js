48f2d66950139700688d6f5d0df5d9f8cc2492c0
Francisco Cesare <franciscocesare@MacBook-Pro-de-Francisco.local>
2022-05-29 21:57:20 -0300

    agregue nodemon a las devDependencies, un contenido muy basico al html, icono en el bropwser y titulo nada mas, y listen en el app.js

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+//aca iran las rutas 
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
