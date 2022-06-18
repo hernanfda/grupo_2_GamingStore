378a26956f39aa037d6d4913eafa099094b683dd
hernanfda <hernanfda@hotmail.com>
2022-06-18 19:09:31 -0300

    Agrego titulos, info, links y estilos sobre cada slide

--- public/js/carrousel.js
+++ public/js/carrousel.js
@@ -1,16 +1,16 @@
 let slideIndex = 1;
-let timer //the timer will get a value when showSlides kicks in
+// let timer //the timer will get a value when showSlides kicks in
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    clearTimeout(timer); //reset timer each time we change slides with the arrows
+    // clearTimeout(timer); //reset timer each time we change slides with the arrows
     showSlides((slideIndex += n));
 }
 
 // dot image controls
 function currentSlide(n) {
-    clearTimeout(timer); //reset timer each time we select a slide manually
+    // clearTimeout(timer); //reset timer each time we select a slide manually
     showSlides((slideIndex = n));
 }
 
@@ -18,7 +18,7 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n === undefined) { slideIndex++ } //Auto start the slideshow
+    // if (n === undefined) { slideIndex++ } //Auto start the slideshow
     //the or.. prevents the auto mechanism to hit an empty index
     if (n > slides.length || slideIndex > slides.length) {
         slideIndex = 1;
@@ -34,5 +34,5 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    timer = setTimeout(showSlides, 16000) //timer starts
+    // timer = setTimeout(showSlides, 16000) //timer starts
 }
