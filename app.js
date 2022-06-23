4edfbbdbfee35b3bb01a0b8610c854123721013d
diegonavarr <diegonavarr@gmail.com>
2022-06-22 22:33:43 -0300

    eliminada las rutas a los demas productos, unificado el footer en todas las vistas

--- app.js
+++ app.js
@@ -27,33 +27,6 @@ app.get("/product_detail_keyboard_redragon", (req, res) => {
   res.sendFile(__dirname + "/views/product_detail_keyboard_redragon.html");
 });
 
-app.get("/product_detail_mouse", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_mouse.html");
-});
-app.get("/product_detail_mouse_logitech", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_mouse_logitech.html");
-});
-app.get("/product_detail_mouse_redragon", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_mouse_redragon.html");
-});
-app.get("/product_detail_gamingchair", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_gamingchair.html");
-});
-app.get("/product_detail_gamingchair_corsair", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_gamingchair_corsair.html");
-});
-app.get("/product_detail_gamingchair_secretlab", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_gamingchair_secretlab.html");
-});
-app.get("/product_detail_headset", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_headset.html");
-});
-app.get("/product_detail_headset_logitech", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_headset_logitech.html");
-});
-app.get("/product_detail_headset_redragon", (req, res) => {
-  res.sendFile(__dirname + "/views/product_detail_headset_redragon.html");
-});
 
 app.get("/product-cart", (req, res) => {
   res.sendFile(__dirname + "/views/product-cart.html");
