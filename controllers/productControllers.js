b8dc1eefa20d3f20f31f0d27412a484189dd1f88
hernanfda <hernanfda@hotmail.com>
2022-07-03 20:08:54 -0300

    Creando objeto de producto

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,3 +1,15 @@
+const productList = [
+    {
+        id: 1,
+        brand: "LOGITECH",
+        model: "G413",
+        type: "keyboard",
+        price: "$10.499",
+        offers: true, //Va a la seccion "en oferta"
+        popular: true, //Va a la seccion "destacados"
+    }
+]
+
 const productControllers = {
     productCart: (req, res) => {
         res.render('products/cart', { styles: "product-cart" })
