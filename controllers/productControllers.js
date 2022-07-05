35ff575472a83c35a9b251f62354d41197286d66
BautistaBoudou <bautyboudou@gmail.com>
2022-07-04 21:55:32 -0300

    Completando products list

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -7,7 +7,70 @@ const productList = [
         price: "$10.499",
         offers: true, //Va a la seccion "en oferta"
         popular: true, //Va a la seccion "destacados"
-    }
+    },
+    {
+        id: 2,
+        brand: "REDDRAGON",
+        model: "FIZZ K617",
+        type: "keyboard",
+        price: "$8.499",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 3,
+        brand: "REDDRAGON",
+        model: "STORM M808",
+        type: "mouse",
+        price: "$6.299",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 4,
+        brand: "LOGITECH",
+        model: "PRO WIRELESS",
+        type: "mouse",
+        price: "$9.499",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 5,
+        brand: "REDDRAGON",
+        model: "H510 ZEUS-X",
+        type: "headset",
+        price: "$10.799",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 6,
+        brand: "LOGITECH",
+        model: "G733",
+        type: "headset",
+        price: "$21.799",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 7,
+        brand: "SECRETLAB",
+        model: "TITANXL",
+        type: "chair",
+        price: "$38.000",
+        offers: true,
+        popular: true, 
+    },
+    {
+        id: 8,
+        brand: "CORSAIR",
+        model: "T3 RUSH",
+        type: "chair",
+        price: "$84.899",
+        offers: true,
+        popular: true, 
+    },
 ]
 
 const productControllers = {
