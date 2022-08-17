09692e8e808376b7dd0d753462a5f476fb576ca4
alex-arango47 <alexis.arango21@gmail.com>
2022-08-17 15:25:46 -0300

    Arreglando login, cookies/session, y mostrar el rebote en vista

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,43 +2,39 @@ const { validationResult } = require("express-validator")
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
 const path = require('path')
-const allUsersPath = path.resolve(__dirname,'../data/users.json');
+const allUsersPath = path.resolve(__dirname, '../data/users.json');
 const allUsers = JSON.parse(fs.readFileSync(allUsersPath, 'utf-8'));
 
 const userControllers = {
+    
     login: (req, res) => {
         res.render('users/login', { styles: "register_login" })
     },
+
     processLogin: (req, res) => {
-        let userToLogin = allUsers.find(user => user.email === req.body.email);
-        console.log(req.body.remember_me)
-        if(userToLogin) {
-            let passwordCheck = bcrypt.compareSync(req.body.password, userToLogin.password);
-            if(passwordCheck) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-                if(req.body.remember_me == "on") {
-                    res.cookie('userEmail', userToLogin.email, { maxAge: (1000 * 60) * 30 });
-                }
-                return res.redirect('/')
+        const userTologin = allUsers.find(userLogged => userLogged.email === req.body.email);
+
+        if (!userTologin || !bcrypt.compareSync(req.body.password, userTologin.password)) {
+            return res.render('users/login', { errors: 'El usuario y/o la clave no coinciden', styles: "register_login" })
+        } else {
+            const user = {...userTologin, password: undefined};
+            Reflect.deleteProperty(user, 'password');
+            req.session.userLogged = user;
+
+            if (req.body.remember_me) {
+                res.cookie('userEmail', userTologin.email, { maxAge: 1000 * 60 * 60 * 24 * 7 })
             }
+            res.redirect('/')
         }
-        return res.render('users/login', {
-            errors:{
-                email:{
-                    msg:'Credenciales inválidas'
-                }
-            },
-            styles: "register_login"
-        })
     },
+
     register: (req, res) => {
         res.render('users/register', { styles: "register_login" })
     },
     processRegister: (req, res) => {
         let errors = validationResult(req)
 
-        if(errors.errors.length > 0) {
+        if (errors.errors.length > 0) {
             return res.render("users/register", {
                 errors: errors.mapped(),
                 old: req.body,
@@ -47,16 +43,9 @@ const userControllers = {
         }
 
         let userExists = allUsers.find(user => user.email === req.body.email);
-        if(userExists) {
-            return res.render('users/register', {
-                errors:{
-                    email:{
-                        msg:'Ya esta registrado este email'
-                    }
-                },
-                styles: "register_login"
-            })
-        }else {
+        if (userExists) {
+            return res.render('users/register', { errors: { email: { msg: 'Ya esta registrado este email' } }, styles: "register_login" })
+        } else {
             let newUser = {
                 nombre: req.body.nombre,
                 apellido: req.body.apellido,
@@ -67,10 +56,10 @@ const userControllers = {
                 userRole: "",
                 id: allUsers.length + 1,
             }
-    
+
             allUsers.push(newUser)
-    
-    
+
+
             fs.writeFileSync("./data/users.json", JSON.stringify(allUsers, null, 2))
             res.render("users/login", { styles: "register_login" });
         }
@@ -81,7 +70,7 @@ const userControllers = {
         res.redirect('/');
     },
     profile: (req, res) => {
-        return res.render('users/profile', {user: req.session.userLogged, styles: "register_login"})
+        return res.render('users/profile', { user: req.session.userLogged, styles: "register_login" })
     },
 }
 
