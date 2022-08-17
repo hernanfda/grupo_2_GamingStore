2a36fc6b2ded70d688a641eab4cc6c0d0ace85f1
BautistaBoudou <bautyboudou@gmail.com>
2022-08-16 22:33:30 -0300

    Se elimino el middleware userLogged de la ruta porque es global

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const userControllers = require('../controllers/userControllers');
 const validator = require('../middlewares/validator');
 const multer = require('../middlewares/multer');
-const userLogged = require('../middlewares/userLogged')
 
-router.get("/login", userLogged ,userControllers.login);
+router.get("/login", userControllers.login);
 router.post("/login", userControllers.processLogin);
 router.get("/register", userControllers.register);
 router.post("/register", multer.single('userAvatar'),validator.register, userControllers.processRegister);
