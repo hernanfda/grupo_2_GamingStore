eacd1be52f2a0bcbe2bc57fdc6670af796fd6b9e
hernanfda <hernanfda@hotmail.com>
2022-09-11 22:48:46 -0300

    Arreglamos vista edit ejs + conectamos update a la base

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -53,7 +53,7 @@ const productControllers = {
     },
     productDetail: async (req, res) => {
         let id = req.params.id;
-        await ProductList.findByPk(id, {include:["brands"]})
+        await ProductList.findByPk(id, { include: ["brands"] })
             .then((product) => {
                 res.render("products/details", {
                     styles: "product_detail_styles",
@@ -88,7 +88,7 @@ const productControllers = {
             category_id: req.body.category_id,
         })
             .then((data) => {
-                res.redirect('products');
+                res.redirect("products");
             })
             .catch((err) => {
                 console.error(err);
@@ -96,32 +96,44 @@ const productControllers = {
     },
     editProduct: async (req, res) => {
         let id = req.params.id;
-        let productForm = ProductList.findByPk(id, {include:["brands", "categories"]});
+        let productForm = ProductList.findByPk(id, { include: ["brands", "categories"] });
         let brandForm = Brands.findAll();
         let categoryForm = Categories.findAll();
+        let selected = "selected";
         Promise.all([categoryForm, brandForm, productForm])
             .then(([categories, brands, product]) => {
                 //console.log(brands);
-                return res.render("products/edit", { styles: "register_login", categories, brands, product });
+                return res.render("products/edit", { styles: "register_login", categories, brands, product, selected });
             })
             .catch((error) => {
                 console.error(error);
             });
     },
-    updateProduct: (req, res) => {
+    updateProduct: async (req, res) => {
         let id = req.params.id;
-        let index = productList.findIndex((e) => e.id == id);
-        productList[index].brand = req.body.brand || productList[index].brand;
-        productList[index].model = req.body.model || productList[index].model;
-        productList[index].type = req.body.type || productList[index].type;
-        productList[index].price = req.body.price || productList[index].price;
-        productList[index].description = req.body.description || productList[index].description;
-        if (req.file) {
-            productList[index].image = req.file.filename;
-        }
+        let product = ProductList.findByPk(id, { include: ["brands", "categories"] });
+        await ProductList.update(
+            {
+                brand_id: req.body.brand_id,
+                model: req.body.model,
+                price: req.body.price,
+                image: req.body.image ? req.body.image : product.image,
+                offer: req.body.offer ? 1 : 0,
+                description: req.body.description,
+                popular: req.body.popular ? 1 : 0,
+                category_id: req.body.category_id,
+            },
+            { where: { id: id } }
+        )
+            .then((data) => {
+                res.redirect("/products");
+            })
+            .catch((err) => {
+                console.error(err);
+            });
 
-        fs.writeFileSync("./data/products-list.json", JSON.stringify(productList, null, 2));
-        res.redirect("/products");
+        // fs.writeFileSync("./data/products-list.json", JSON.stringify(productList, null, 2));
+        // res.redirect("/products");
     },
     deleteProduct: (req, res) => {
         let id = req.params.id;
@@ -131,6 +143,5 @@ const productControllers = {
         res.redirect("/products");
     },
 };
-
 
 module.exports = productControllers;
