58a209040494a60968140ad4c4c8178d3730e8c3
hernanfda <hernanfda@hotmail.com>
2022-09-17 23:18:32 -0300

    Arreglados los problemas de autenticacion de admin

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,11 +1,11 @@
 module.exports = function adminMiddleware(req, res, next) {
-    console.log(req.session.userLogged.userRole)
-    if(!req.session.userLogged) {
-        return res.redirect('/users/login');
-    }
-    else if(req.session.userLogged && req.session.userLogged.userRole != 2) {
-        console.log("no es admin")
-        return res.redirect('/');
+    console.log(req.session.userLogged.dataValues.user_profile_id);
+    if (!req.session.userLogged) {
+        return res.redirect("/users/login");
+    } else if (req.session.userLogged && req.session.userLogged.dataValues.user_profile_id != 2) {
+        console.log(req.session.userLogged);
+        console.log("no es admin");
+        return res.redirect("/");
     }
     next();
-}
+};
