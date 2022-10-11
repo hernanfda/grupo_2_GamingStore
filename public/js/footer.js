9b97e75989956eedbb83d7d870aef3ec5b130eb9
alex-arango47 <alexis.arango21@gmail.com>
2022-10-11 16:41:10 +1300

    fixing footer styling in broken views, adding js logic so the footer doesn't bug on categories

---
+++ public/js/footer.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+    let section = document.querySelector('section')
+    let div = document.querySelector('#true')
+    if(div){
+        section.style.height = '100vh'
+    }
+})
