2eb486242764bec6fa1c3b7992ae05fc8ab85a78
alex-arango47 <alexis.arango21@gmail.com>
2022-10-11 17:01:15 +1300

    change logic for footer styling to only use js and not ejs

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function(){
+    let articulos = document.querySelectorAll(".articulo")
+    // console.log(articulos.length)
     let section = document.querySelector('section')
-    let div = document.querySelector('#true')
-    if(div){
+    // let div = document.querySelector('#true')
+    if(articulos.length < 8){
         section.style.height = '100vh'
     }
 })
