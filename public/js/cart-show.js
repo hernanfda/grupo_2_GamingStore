75071504ca7e403db793f32c5900068431335c56
hernanfda <hernanfda@hotmail.com>
2022-10-13 22:23:26 -0300

    Agregamos funcionalidad al carrito, menos delete y botones de cantidad

--- public/js/cart-show.js
+++ public/js/cart-show.js
@@ -1,39 +1,63 @@
 window.addEventListener("load", (e) => {
     const cartDetail = document.querySelector(".cart-content_products"); //es todo el div que contiene el cart
-    const thrashCart = document.querySelector(".content-empty_btn");
-    const deleteProduct = document.querySelector(".actions-form_delete");
+    const trashCart = document.querySelector(".content-empty_btn");
 
     let articulosCarrito = JSON.parse(localStorage.getItem("cart")) || [];
     console.log(articulosCarrito);
     showCart();
 
     function showCart() {
+        const productPrices = [];
         articulosCarrito.forEach((producto) => {
-            (document.querySelector(".product-title").innerHTML = producto.brand),
-                (document.querySelector(".product-title").innerHTML += producto.model),
-                (document.querySelector(".product-price").innerHTML = producto.price);
-            document.querySelector(".products-details_img").setAttribute("src", producto.image);
-            // id: producto.id
+            productPrices.push(producto.price);
+            document.querySelector(".cart-content_products").innerHTML += `<div class='products-details'>
 
-            console.log(producto.image);
+                <img class="products-details_img" src="${producto.image}">
+                <div class="products-details_text">
+                    <div class='product-title'>${producto.brand}</div>  
+                    <div class='product-title'>${producto.model}</div>  
+                    <div class='product-price'>${producto.price}</div>
+                    <div class="cart-content_actions">
+                    <form class="actions-form">
+                        <div class="actions-form_left">
+                            <button class="actions-form_delete" id="${producto.id}">Eliminar Producto</button>
+                        </div>
+                        <div class="actions-form_right">
+                            <button class="actions-form_btn">&minus;</button>
+                            <input type="number" class="actions-form_input" name="qty">
+                            <button class="actions-form_btn">&plus;</button>
+                        </div>
+                    </form>
+                    <div class="actions-line"></div>
+                </div>
+                </div>  
+            </div>`;
         });
+
+        console.log(productPrices);
+        let initialValue = 0;
+        const sumWithInitial = productPrices.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+
+        document.querySelector(".summary-number_total").innerText = `$${sumWithInitial}`;
     }
 
     //trash cart is correctly removed
-    thrashCart.addEventListener("click", (e) => {
-        e.preventDefault();
+    trashCart.addEventListener("click", () => {
         //  alert('delete cart');
-        localStorage.clear("cart");
-        location.reload();
+        // e.preventDefault();
+        console.log("delete de panchito");
+        // localStorage.clear("cart");
+        // console.log(localStorage);
     });
 
-    deleteProduct.addEventListener("click", (e) => {
-        if (e.target.classList.contains("actions-form_delete")) {
-            e.preventDefault();
-            const product = e.target.parentElement.parentElement;
-            const productId = product.querySelector("a").getAttribute("data-product-id");
-            cartIcon.animate(cartSpinning, timeSpinning);
-        }
+    const deleteProduct = document.querySelectorAll(".actions-form_delete");
+
+    deleteProduct.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            let newCart = articulosCarrito.filter((element) => element.id != parseInt(e.target.id));
+            localStorage.setItem("cart", JSON.stringify(newCart));
+        });
     });
+
     //console.log(articulosCarrito);
 });
