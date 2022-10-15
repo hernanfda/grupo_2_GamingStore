4deb7d0ff1758427644cfeee2051a5cc7d3f9c28
hernanfda <hernanfda@hotmail.com>
2022-10-15 19:54:36 -0300

    Agregada la funcionalidad a los botones +/- del carrito

--- public/js/cart-show.js
+++ public/js/cart-show.js
@@ -9,23 +9,33 @@ window.addEventListener("load", (e) => {
     function showCart() {
         const productPrices = [];
         articulosCarrito.forEach((producto) => {
-            productPrices.push(producto.price);
+            let productQty = producto.cantidad;
+            console.log(productQty);
+            if (productQty > 1) {
+                console.log("entre al if de mas de 1");
+                while (productQty >= 1) {
+                    productPrices.push(producto.price);
+                    productQty--;
+                }
+            } else {
+                productPrices.push(producto.price);
+            }
             document.querySelector(".cart-content_products").innerHTML += `<div class='products-details'>
 
                 <img class="products-details_img" src="${producto.image}">
                 <div class="products-details_text">
                     <div class='product-title'>${producto.brand}</div>  
                     <div class='product-title'>${producto.model}</div>  
-                    <div class='product-price'>${producto.price}</div>
+                    <div class='product-price'>$${producto.price}</div>
                     <div class="cart-content_actions">
                     <form class="actions-form">
                         <div class="actions-form_left">
                             <button class="actions-form_delete" id="${producto.id}">Eliminar Producto</button>
                         </div>
                         <div class="actions-form_right">
-                            <button class="actions-form_btn">&minus;</button>
-                            <input type="number" class="actions-form_input" name="qty">
-                            <button class="actions-form_btn">&plus;</button>
+                            <button class="actions-form_btn" id="minus_btn">&minus;</button>
+                            <input type="number" class="actions-form_input" id=${producto.id} name="qty" value=${producto.cantidad} min="1">
+                            <button class="actions-form_btn" id="plus_btn">&plus;</button>
                         </div>
                     </form>
                     <div class="actions-line"></div>
@@ -33,27 +43,55 @@ window.addEventListener("load", (e) => {
                 </div>  
             </div>`;
         });
-
         console.log(productPrices);
-        let initialValue = 0;
-        const sumWithInitial = productPrices.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+        totalValue();
 
-        document.querySelector(".summary-number_total").innerText = `$${sumWithInitial}`;
-    }
+        //Cart qty buttons
+        const plusBtn = document.querySelectorAll("#plus_btn");
+        const minusBtn = document.querySelectorAll("#minus_btn");
+        const qtyField = document.querySelectorAll(".actions-form_input");
 
-    //trash cart is correctly removed
-    document.querySelector(".content-empty_btn").addEventListener("click", () => {
-        localStorage.clear("cart");
-        location.reload();
-    });
-    const deleteProduct = document.querySelectorAll(".actions-form_delete");
-
-    deleteProduct.forEach((button) => {
-        button.addEventListener("click", (e) => {
-            let newCart = articulosCarrito.filter((element) => element.id != parseInt(e.target.id));
-            localStorage.setItem("cart", JSON.stringify(newCart));
+        plusBtn.forEach((button, i) => {
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+                articulosCarrito[i].cantidad += 1;
+                qtyField[i].value = articulosCarrito[i].cantidad;
+                localStorage.setItem("cart", JSON.stringify(articulosCarrito));
+                location.reload();
+            });
         });
-    });
+
+        minusBtn.forEach((button, i) => {
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+                articulosCarrito[i].cantidad -= 1;
+                qtyField[i].value = articulosCarrito[i].cantidad;
+                localStorage.setItem("cart", JSON.stringify(articulosCarrito));
+                location.reload();
+            });
+        });
+
+        //trash cart is correctly removed
+        document.querySelector(".content-empty_btn").addEventListener("click", () => {
+            localStorage.clear("cart");
+            location.reload();
+        });
+        const deleteProduct = document.querySelectorAll(".actions-form_delete");
+
+        deleteProduct.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                let newCart = articulosCarrito.filter((element) => element.id != parseInt(e.target.id));
+                localStorage.setItem("cart", JSON.stringify(newCart));
+            });
+        });
+
+        //Total value function
+        function totalValue() {
+            let initialValue = 0;
+            const sumWithInitial = productPrices.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+            document.querySelector(".summary-number_total").innerText = `$${sumWithInitial}`;
+        }
+    }
 
     //console.log(articulosCarrito);
 });
