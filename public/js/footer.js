3eee098dc7d3fb4666d8cf295563950c271f5104
diegonavarr <diegonavarr@gmail.com>
2022-10-17 21:25:14 -0300

    cambiando el js de footer por height 100%

--- public/js/footer.js
+++ public/js/footer.js
@@ -4,6 +4,6 @@ window.addEventListener('load', function(){
     let section = document.querySelector('section')
     // let div = document.querySelector('#true')
     if(articulos.length < 8){
-        section.style.height = '100vh'
+        section.style.height = '100%'
     }
 })
