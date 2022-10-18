93edfec890dca95dd6024cfd26ccd0a1cf9b797a
diegonavarr <diegonavarr@gmail.com>
2022-10-17 23:22:54 -0300

    corrigiendo js de footer

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,8 +2,13 @@ window.addEventListener('load', function(){
     let articulos = document.querySelectorAll(".articulo")
     // console.log(articulos.length)
     let section = document.querySelector('section')
+    let body = document.querySelector('body')
+    let footer= document.querySelector('footer')
     // let div = document.querySelector('#true')
     if(articulos.length < 8){
         section.style.height = '100%'
+        body.style.position = "unset"
+        footer.style.position ="absolute";
+        footer.style.bottom = "0px"
     }
 })
